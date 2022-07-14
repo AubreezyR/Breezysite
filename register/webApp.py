47ca74d630849621cf8e6ce6823800ece6294901
Auburn Robinson <alrobinson4@dons.usfca.edu>
2022-07-14 15:54:19 +0000

    adding a background and more style to my website

--- register/webApp.py
+++ register/webApp.py
@@ -10,8 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(20), unique=True, nullable=False)
+  id = db.Column(db.Integer, primary_key=True)  username = db.Column(db.String(20), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   password = db.Column(db.String(60), nullable=False)
 
